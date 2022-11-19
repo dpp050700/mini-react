af4b74fc42916cbdf2c8452c86ecbda74375eaaf
戴鹏鹏 <16602124116@163.com>
2022-11-19 15:25:10 +0800

    初始化项目工程

--- demos/vite.config.ts
+++ demos/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   resolve: {
     alias: [
       {
